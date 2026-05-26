6bb08caf78e726baac4ae5b55b2b8261c697dbe3
chinhhoangtc <chinhhoangtc03@gmail.com>
2026-05-26 09:31:52 +0700

    fix

--- src/sections/tour/components/booking-form.tsx
+++ src/sections/tour/components/booking-form.tsx
@@ -207,7 +207,7 @@ const BookingForm = ({ item }: Props) => {
                 queryClient.invalidateQueries({
                     queryKey: [QUERY_KEYS.CART.LIST_CART],
                 });
-                route.push(paths.cart.list);
+                route.replaceParams(paths.cart.list, { item: item, price: price, payload: buildPayload(), });
                 showToast("success", "Thêm vào giỏ thành công");
             },
             onError: () => {
