b8a81bb3614fa71e1a5023c8b025df54bb393db7
chinhhoangtc <chinhhoangtc03@gmail.com>
2026-06-06 14:35:20 +0700

    fix

--- src/sections/tour/components/tour-location-des.tsx
+++ src/sections/tour/components/tour-location-des.tsx
@@ -30,21 +30,6 @@ const TourLocationDes = ({
      * undefined
      * {}
      */
-    const isValidValue = (value: any) => {
-        if (value == null) {
-            return false;
-        }
-
-        if (
-            typeof value === "object" &&
-            !Array.isArray(value) &&
-            Object.keys(value).length === 0
-        ) {
-            return false;
-        }
-
-        return true;
-    };
 
 
     const isRealString = (v: any) =>
