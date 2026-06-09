08da3b6e2d62e2f70789766a3231114f1f24d03e
chinhhoangtc <chinhhoangtc03@gmail.com>
2026-06-09 15:36:15 +0700

    fix

--- src/sections/shop/components/shop-header.tsx
+++ src/sections/shop/components/shop-header.tsx
@@ -208,7 +208,11 @@ const ShopHeader = () => {
                         <Notification />
 
                         <button
-                            onClick={() => window.open("https://myagentmember.itourlink.com/request-booking", "_blank")}
+                            onClick={() =>
+                                router.push(
+                                    `${paths.shop.agentCompany.list}?company=${company}`
+                                )
+                            }
                             className="cursor-pointer rounded-lg border border-[rgba(64,64,64,0.5)] px-3 py-2 text-[14px] font-medium text-gray-700 hover:text-[#2566b0] hover:bg-blue-50 transition-all duration-200 active:scale-95"
                         >
                             {t("listAgentHost")}
@@ -217,11 +221,7 @@ const ShopHeader = () => {
                         <TourCustomized />
 
                         <button
-                            onClick={() =>
-                                router.push(
-                                    `${paths.shop.agentCompany.list}?company=${company}`
-                                )
-                            }
+                            onClick={() => window.open("https://myagentmember.itourlink.com/request-booking", "_blank")}
                             className="cursor-pointer rounded-lg border border-[rgba(64,64,64,0.5)] px-3 py-2 text-[14px] font-medium text-gray-700 hover:text-[#2566b0] hover:bg-blue-50 transition-all duration-200 active:scale-95"
                         >
                             {t("myRequest")}
