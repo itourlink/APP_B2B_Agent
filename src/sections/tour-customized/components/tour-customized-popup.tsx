cdd0789aade5d5b5efe4307cde81dd4383dacf6a
Duong Van Ngoc <duongvanngoc150903@gmail.com>
2026-06-11 13:46:12 +0700

    11 -06

--- src/sections/tour-customized/components/tour-customized-popup.tsx
+++ src/sections/tour-customized/components/tour-customized-popup.tsx
@@ -27,13 +27,20 @@ type Props = {
 
 const TourCustomizedPopup = ({ onClose }: Props) => {
     const { t } = useTranslate("tourcustomize");
+    const today = new Date().toISOString().split("T")[0]
+
 
     const Schema = z
         .object({
             agentHost: z.string().min(1, t("selectAgentHost")),
             currency: z.string().min(1, t("selectCurrency")),
             tourName: z.string().min(1, t("tourNameRequired")),
-            dateStart: z.string().min(1, t("startDateRequired")),
+            dateStart: z
+                .string()
+                .min(1, t("startDateRequired"))
+                .refine((value) => value >= today, {
+                    message: t("dayNoti")
+                }),
 
             nationality: z.string().min(1, t("selectNationality")),
 
@@ -401,6 +408,7 @@ const TourCustomizedPopup = ({ onClose }: Props) => {
                 <Field.Text
                     name="dateStart"
                     type="date"
+                    min={today}
                     label={{
                         text: t("dateStart"),
                         icon: <span className="text-red-500">*</span>,
