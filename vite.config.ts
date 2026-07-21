5e0301a141b6a4df52f281b2f32768034b791a2d
pngdev4 <itourlink@gmail.com>
2026-07-21 15:44:55 +0700

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
